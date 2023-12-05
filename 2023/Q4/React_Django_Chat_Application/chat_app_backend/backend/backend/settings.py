c61a46b48d7879f8f8107a53e69f6614497a3638
SujanTuladhar-Outcode <sujan.tuladhar@outcodesoftware.com>
2023-12-05 09:49:53 +0545

    feature/django-react-chat-app [New]: Dockerize Application

--- 2023/Q4/React_Django_Chat_Application/chat_app_backend/backend/backend/settings.py
+++ 2023/Q4/React_Django_Chat_Application/chat_app_backend/backend/backend/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "media/"
+
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
