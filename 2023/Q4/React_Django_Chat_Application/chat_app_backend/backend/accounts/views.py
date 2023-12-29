2304fbc255437e3e8a950733c88cb602956d14ca
SujanTuladhar-Outcode <sujan.tuladhar@outcodesoftware.com>
2023-12-29 09:51:10 +0545

    feature/django-react-chat-app [Update]: Register User View

--- 2023/Q4/React_Django_Chat_Application/chat_app_backend/backend/accounts/views.py
+++ 2023/Q4/React_Django_Chat_Application/chat_app_backend/backend/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
