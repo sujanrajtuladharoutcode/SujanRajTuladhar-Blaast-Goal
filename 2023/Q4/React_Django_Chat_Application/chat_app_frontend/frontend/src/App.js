7771a920906ec228d652b2bb1ffd1383df614e5a
SujanTuladhar-Outcode <sujan.tuladhar@outcodesoftware.com>
2024-01-25 16:47:04 +0545

    feature/django-react-chat-app [New]: Login page

--- 2023/Q4/React_Django_Chat_Application/chat_app_frontend/frontend/src/App.js
+++ 2023/Q4/React_Django_Chat_Application/chat_app_frontend/frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Register from './Components/Register';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <Register/>
+    <Login/>
   );
 }
 
